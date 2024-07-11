e6903eacf528fe958f234878677ef664f5b69971
jakafind <jakafind.id@gmail.com>
2024-07-11 13:25:18 +0600

    Create 02. Print Character Text.java

---
+++ 02. Print Character Text.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    System.out.println("Hello Jakaria!");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+    
+  }
+}
