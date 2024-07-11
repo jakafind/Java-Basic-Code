33ea543b492fcc3345a3dc7afe34f475538897d7
jakafind <jakafind.id@gmail.com>
2024-07-11 13:27:42 +0600

    Create 03. Print Natural Numbers (1-20) using Loop.java

---
+++ 03. Print Natural Numbers (1-20) using Loop.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Print Natural Number (1-20): \n");
+
+        for (int i = 1; i <= 20; i++) {
+
+            System.out.print("   " + i);
+        }
+    }
+}
